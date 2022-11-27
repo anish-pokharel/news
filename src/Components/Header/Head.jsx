f7b98ebcacce2d36abb9315811143accfd54390f
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-11-28 01:07:43 +0545

    news header

---
+++ src/Components/Header/Head.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../../images/logo.png";
+import logo1 from "../../images/headerb.png";
+
+const Head = () => {
+  return (
+    <>
+      <section className="head">
+        <div className="container flexSB paddingTB">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="ad">
+            <img src={logo1} alt="" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Head;
